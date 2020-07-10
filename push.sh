bcd60ce1ce0e5aee4fb73be66cf0f95fa221e96c
goat <87699026@qq.com>
2020-07-10 15:53:48 +0800

    lazy1.0.0

--- push.sh
+++ push.sh
@@ -3,11 +3,11 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-#git init
+git init
 git add .
 git commit -m $1
 
-#git remote add origin https://github.com/91xqk/lazyscript.git
+git remote add origin https://github.com/91xqk/lazyscript.git
 
 git push -f origin master
 
