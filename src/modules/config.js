416b382de46f01e1798796ea7be469311e4e20cb
goat <87699026@qq.com>
2020-07-29 18:26:53 +0800

    调整

--- src/modules/config.js
+++ src/modules/config.js
@@ -2,12 +2,14 @@ iKnow.define('config', function (require, module, exports) {
     //作者
     exports.author='joy'
     //版本信息
-    exports.verison='1.0.0'
+    exports.verison='1.0.1'
     //是否debug模式
     exports.debugger = true
     //初始化加载模块，以空格隔开
     exports.USEMODULES='common md5 datetime extend layer'
     //域名
     exports.HOST=''
+    //
+    exports.ProjectInfo='iKnow'
 
 })
